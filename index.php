2e3b582e786a9d6c1891cea5b7fb38b3e9853505
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2016-05-09 22:39:11 +0200

    Added some styling

--- index.php
+++ index.php
@@ -3,9 +3,51 @@
   <head>
     <meta charset="utf-8">
     <title>Harvest Money</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/styles/output/main.css">
   </head>
   <body>
-    <h1>Hello world</h1>
+
+    <div class="wrapper">
+
+        <h1>Harvest Money</h1>
+
+        <section class="project">
+            <header>
+                <h2>Project - Client</h2>
+            </header>
+
+            <div class="table">
+                <div class="row">
+                    <div class="task">Task</div>
+                    <div class="hours">1,23 uur</div>
+                    <div class="price">€ 123,40</div>
+                </div>
+
+                <div class="row">
+                    <div class="task">Task</div>
+                    <div class="hours">1,23 uur</div>
+                    <div class="price">€ 123,40</div>
+                </div>
+
+                <div class="row">
+                    <div class="task">Task</div>
+                    <div class="hours">1,23 uur</div>
+                    <div class="price">€ 123,40</div>
+                </div>
+
+                <div class="row total">
+                    <div class="task">&nbsp;</div>
+                    <div class="hours">12,34 uur</div>
+                    <div class="price">€ 1.234,56</div>
+                </div>
+            </div>
+
+            <footer>
+                <a class="button" href="#">Factureer uren</a>
+            </footer>
+        </section>
+
+    </div>
+
   </body>
 </html>
