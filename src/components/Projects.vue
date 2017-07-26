c5433a32effcc4098b5e6cf0b0fcf20aa9b02f05
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-07-26 23:01:53 +0200

    :construction: Harvest class

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+  import Harvest from '../helpers/harvest.js';
   import Project from './Project.vue';
 
   export default {
@@ -18,39 +19,13 @@
 
     data() {
       return {
-        projects: [
-          {
-            id: 1,
-            name: "Project naam",
-            uninvoiced: 53.5,
-            link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
-          },
-          {
-            id: 1,
-            name: "Nog ene project",
-            uninvoiced: 53.5,
-            link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
-          },
-          {
-            id: 1,
-            name: "Derde project",
-            uninvoiced: 53.5,
-            link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
-          },
-          {
-            id: 1,
-            name: "Project",
-            uninvoiced: 53.5,
-            link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
-          },
-          {
-            id: 1,
-            name: "Project",
-            uninvoiced: 53.5,
-            link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
-          }
-        ]
+        projects: false,
       }
-    }
+    },
+
+    mounted() {
+      const harvest = new Harvest();
+      this.projects = harvest.getProjects();
+    },
   }
 </script>
---
+++ src/helpers/harvest.js
@@ -0,0 +1,40 @@
+export default class Harvest {
+  // constructor() {
+  //   this.apiKey = 'ABCD';
+  // }
+
+  getProjects() {
+    return [
+      {
+        id: 1,
+        name: "Project naam",
+        uninvoiced: 53.5,
+        link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
+      },
+      {
+        id: 1,
+        name: "Nog ene project",
+        uninvoiced: 53.5,
+        link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
+      },
+      {
+        id: 1,
+        name: "Derde project",
+        uninvoiced: 53.5,
+        link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
+      },
+      {
+        id: 1,
+        name: "Project",
+        uninvoiced: 53.5,
+        link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
+      },
+      {
+        id: 1,
+        name: "Project",
+        uninvoiced: 53.5,
+        link: "https://www.youtube.com/embed/-0kcet4aPpQ?autoplay=1",
+      }
+    ];
+  }
+}
