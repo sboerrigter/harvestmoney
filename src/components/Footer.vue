4fa125cbbfcae593b49b3257a761200498fe9203
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-07-26 21:49:52 +0200

    :fire: Obsolete container div in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,10 @@
 <template>
   <footer class="hero is-light">
-    <div class="hero-body">
-      <div class="container">
-        <div class="content has-text-centered">
-          <p>
-            Harvest Money is buildt by <strong><a :href="url" target="_blank">{{ author }}</a></strong> to make his own live easier :-)
-          </p>
-        </div>
+    <div class="hero-body container">
+      <div class="content has-text-centered">
+        <p>
+          Harvest Money is buildt by <strong><a :href="url" target="_blank">{{ author }}</a></strong> to make his own live easier :-)
+        </p>
       </div>
     </div>
   </footer>
