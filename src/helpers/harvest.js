c80e1feecc39916dc181de3ed041c73381920fbf
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-07-27 15:46:48 +0200

    :sparkles: Get new access token on Harvest API error

--- src/helpers/harvest.js
+++ src/helpers/harvest.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import env from '../../env.js';
 
-export default class Harvest {
-
+export default class Harvest
+{
   constructor() {
     this.account = env.HARVEST_ACCOUNT; // Should be declared in env.js
     this.clientId = env.HARVEST_CLIENT_ID; // Should be declared in env.js
@@ -15,24 +15,28 @@ export default class Harvest {
   }
 
   authenticate() {
-    if (localStorage.getItem('harvest_access_token') !== null) {
-      /* Get access token from LocalStorage */
-      this.accessToken = localStorage.getItem('harvest_access_token');
-    } else if (document.location.hash) {
+    if (document.location.hash) {
       /* Get access token from URL and save it to LocalStorage */
       this.accessToken = document.location.hash.split('access_token=')[1].split('&')[0];
       localStorage.setItem('harvest_access_token', this.accessToken);
+    } else if (localStorage.getItem('harvest_access_token') !== null) {
+      /* Get access token from LocalStorage */
+      this.accessToken = localStorage.getItem('harvest_access_token');
     } else {
-      /* Redirect to Harvest authentication page */
-      document.location.replace(
-        this.baseUrl
-        + '/oauth2/authorize?client_id='
-        + this.clientId
-        + '&redirect_uri='
-        + document.location.origin
-        + '&response_type=token'
-      );
+      /* Get new access token */
+      this.getAccessToken();
     }
+  }
+
+  getAccessToken() {
+    document.location.replace(
+      this.baseUrl
+      + '/oauth2/authorize?client_id='
+      + this.clientId
+      + '&redirect_uri='
+      + document.location.origin
+      + '&response_type=token'
+    );
   }
 
   get(endpoint) {
@@ -45,6 +49,8 @@ export default class Harvest {
       }
     }).then(response => {
       return response.data;
+    }).catch(error => {
+      this.getAccessToken();
     });
   }
 
