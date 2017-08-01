a79b38feb845c8986258e2e1b6aea644d3190090
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2017-08-01 21:14:14 +0200

    :construction: Authenticate to Harvest

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Harvest from './lib/harvest.js';
+
+Harvest.authenticate();
 
 new Vue({
   el: '#app',
