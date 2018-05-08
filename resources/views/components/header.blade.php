43241c01120696c5ab3640e39e46ecfc506540e0
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-05-08 16:29:47 +0200

    :construction: Header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<header class="hero is-dark">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        🚜💰 Harvest Money
+      </h1>
+      <h2 class="subtitle">
+        Maak MoneyBird facturen van Harvest reports
+      </h2>
+    </div>
+  </div>
+</header>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,16 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <title>Harvest Money</title>
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Harvest Money</title>
+        <link rel="stylesheet" href="{{ URL::asset('../node_modules/bulma/css/bulma.css') }}">
     </head>
     <body>
+        @include('components/header')
         @yield('content')
     </body>
 </html>
