bb544018b4f014d6e1d783af5208e84ec7117ec9
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-05-08 16:33:27 +0200

    :sparkles: Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="hero is-dark">
+    <div class="hero-body container">
+      <div class="content has-text-centered">
+        <p>
+          Harvest Money is gemaakt door <strong><a href="http://sjoerdboerrigter.nl/" target="_blank">Sjoerd Boerrigter</a></strong>  om zijn eigen leven makkelijker te maken 😊
+        </p>
+      </div>
+    </div>
+  </footer>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,5 +12,6 @@
     <body>
         @include('components/header')
         @yield('content')
+        @include('components/footer')
     </body>
 </html>
