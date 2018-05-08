2e944b8400cb5b41e6567e0a8f63de3060ec00d0
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2018-05-08 16:31:21 +0200

    :heavy_plus_sign: Load Bulma via CDN

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{ URL::asset('../node_modules/bulma/css/bulma.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
     </head>
     <body>
         @include('components/header')
